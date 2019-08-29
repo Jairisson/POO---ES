e53d7867594d99f738abfcbac9233e4db96a5f15
Jairisson <jairisson99@gmail.com>
2019-08-29 16:52:30 -0300

    Primeiro projeto enviado pro Github

---
+++ aprendendo/src/aprendendo/Principal.java
@@ -0,0 +1,17 @@
+package aprendendo;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Meu primeiro frame em Java");
+		  Painel meuPainel = new Painel();
+		  
+		  janela.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		  janela.add(meuPainel);
+		  janela.setSize(600,400);
+		  janela.setVisible(true);
+		 }
+	}
